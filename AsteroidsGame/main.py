abfd146269383feeac316eb0f01e706f34a95e38
kozakini <wojkom777@protonmail.com>
2025-07-24 17:13:15 +0200

    Framerate added

--- AsteroidsGame/main.py
+++ AsteroidsGame/main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print("Starting Asteroids!")
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt= time.tick(60) / 1000
 
 
 if __name__ == "__main__":
